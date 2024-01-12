e968cec9ef39c824a189fa156c2a8694b8c22253
Tipstat2405 <35723675+Tipstat2405@users.noreply.github.com>
2024-01-12 08:57:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ res.send("<h2>Welcome to Node JS express application</h2>"+appEnv.url+appEnv.por
 }).listen(9009);
 console.log(__dirname+"/images/mithunlogo.jpg");
 */
-app.get('/mithuntechnologies', function(request, response) {
+app.get('/get', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
-    response.write("<h2><center><u>Node JS  Application </u></center></h2>");
+    response.write("<h2><center><u>POC Node JS  Application </u></center></h2>");
 	
-    response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226,++91-9980923216 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
+    response.write("<h2><center>Welcome to  HCL Technologies <center></h2>" );
     response.end();
     
   })
@@ -44,10 +44,9 @@ app.get("/html", function(req,res){
         //res.type('application/json');
         //res.json({'name': 'Mithun Reddy L'});
         res.send({
-		'name': 'Mithun Technologies',
-		'technology': 'DevOps',
-		'contact' : '9980923226',
-		'email': 'devopstrainingblr@gmail.com'
+		'name': 'HCL Technologies',
+		'technology': 'JAVA FULLSTACK',
+		'contact' : '9398555699'
 	            });
         
         });
